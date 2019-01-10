7af51bc79137bb0692e556ab3f52e2715e635018
akumar-99 <ashish.dav99@gmail.com>
2019-01-11 02:06:20 +0530

    Datatype HW 1 Update

---
+++ datatypes/src/Program5.java
@@ -0,0 +1,15 @@
+public class Program5 {
+	public static void main(String[] args) {
+    	int num1 = 12;
+    	int num2 = 10;
+    	int sum = num1 + num2;
+    	int difference = num1 - num2;
+    	float quotient = num1 / num2;
+    	float remainder = num1 % num2;
+    	System.out.println("Number are : " + num1 + " and " + num2);
+		System.out.println("Sum of these numbers: " + sum);
+		System.out.println("Difference of these numbers: " + difference);
+		System.out.println("Quotient of these numbers: " + quotient);
+		System.out.println("Remainder of these numbers: " + remainder);
+    }
+} 	
