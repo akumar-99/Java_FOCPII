2a006cc1566dad8a3fc1a52444e209da5cf6c869
akumar-99 <ashish.dav99@gmail.com>
2019-01-12 00:16:28 +0530

    datatypes and cStatements

--- controlStatements/src/Program4.java
+++ controlStatements/src/Program4.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Program4 {
     public static void main (String[] args) { 
         Scanner input = new Scanner (System.in);
-        char ch; //for re running the code
-        int no; //number entered by the user
+        char ch;    //for re running the code
+        int no;     //number entered by the user
         int digit1; //1st digit of number
         int digit2; //2nd digit
         int digit3; //3rd digit
         int digit4; //4th digit
         int digit5; //5th digit   
         do {      
-            System.out.println("Enter An Integeral Number Consisting of Five Digits : ");
+            System.out.print("Enter An Integeral Number Consisting of Five Digits : ");
             no = input.nextInt();
             digit5 = no % 10;
             digit4 = (no % 100)/10;
--- controlStatements/src/Program5.java
+++ controlStatements/src/Program5.java
@@ -5,15 +5,17 @@ public class Program5 {
         Scanner input = new Scanner (System.in);
         int i; 			//for loop
         int n;			//number of terms
+        int factorial = 1;
         double x;		//for equation
-        double sum=1;	//for result of equation  
-        System.out.println("EQUATION :: 1+x+x^2/2+x^3/3+...+x^n/n");
-        System.out.println("Enter value of x :");
+        double sum = 1;	//for result of equation  
+        System.out.println("EQUATION :: 1 + x + x^2/2! + x^3/3! + ... + x^n/n! ");
+        System.out.print("Enter value of x :");
         x = input.nextDouble();
-        System.out.println("Enter number of terms,n :");
+        System.out.print("Enter number of terms,n :");
         n = input.nextInt();
         for (i=1; i<=n; ++i){
-			sum += Math.pow(x,i) / i;
+            factorial *= i;
+			sum += Math.pow(x,i) / factorial;
         }
         System.out.println("Sum of the EQUATION = " + sum);
     }
